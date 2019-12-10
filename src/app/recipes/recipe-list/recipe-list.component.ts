a50c8cf318fe2fafadd9bdc040d9c65d0ca4fbaa
santoshkbrj <santoshsah017@gmail.com>
2019-12-10 11:26:18 +0530

    Sliptting of all component

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Recipe', 'Tis is simple recipe','https://static01.nyt.com/images/2013/06/26/dining/26JPFLEX1/26JPFLEX1-articleLarge-v3.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
